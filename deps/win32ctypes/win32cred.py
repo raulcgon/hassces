470f7279fb6dfe28a78ea42788da4dc6a2386c3b
Raul Gonzalez <rgonalez@cesantoni.com.mx>
2017-06-16 09:37:51 -0500

    rev. 001

---
+++ deps/win32ctypes/win32cred.py
@@ -0,0 +1,15 @@
+#
+# (C) Copyright 2014 Enthought, Inc., Austin, TX
+# All right reserved.
+#
+# This file is open source software distributed according to the terms in
+# LICENSE.txt
+#
+import warnings
+from win32ctypes.pywin32 import win32cred
+
+warnings.warn(
+    "Please use 'from win32ctypes.pywin32 import win32cred'",
+    DeprecationWarning)
+
+__all__ = ['win32cred']
